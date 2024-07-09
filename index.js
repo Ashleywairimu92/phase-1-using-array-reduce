aa5bfbfd39641f9e7854b16a3ccd1b12fe6110be
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-07-09 19:23:14 +0300

    changes

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// function totalBatteries(batteries){
+//     return batteries = batteryBatches([]);
+// }   
+
+// function totalBatteries(batteryBatches){
+//     return batteryBatches.reduce((total, batch) => totat + batch, 0);
+// }
+
+
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+
